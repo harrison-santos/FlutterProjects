99114a3ec26d1b4fe9ba25cbbd47892847a83054
harrison-santos <harrisonspeed1997@gmail.com>
2020-04-08 21:06:20 -0300

    Primeiro commit

---
+++ conversor_de_moedas/lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';//ajuda na hora de fazer requisições assíncronas
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance";
+
+void main() async {
+  print(await getData());
+
+  runApp(MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+            hintStyle: TextStyle(color: Colors.amber),
+          ))));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);//await espera os dados chegarem
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realChanged(String text) {
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+
+    print(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text("\$ Conversor de Moedas \$"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Center();
+                case ConnectionState.waiting:
+                  return Center(
+                      child: Text(
+                    "Carregando dados",
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ));
+                case ConnectionState.active:
+                  return Center();
+                case ConnectionState.done:
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(Icons.monetization_on,
+                              size: 150, color: Colors.amber),
+                          buildTextField(
+                              "Reais", "R\$", realController, _realChanged),
+                          Divider(),
+                          buildTextField("Dólares", "US\$", dolarController,
+                              _dolarChanged),
+                          Divider(),
+                          buildTextField(
+                              "Euros", "\€", euroController, _euroChanged),
+                        ],
+                      ));
+                default:
+                  if (snapshot.hasError) {
+                    return Center(
+                        child: Text(
+                          "Erro na hora de Carregar os dados",
+                          style: TextStyle(color: Colors.amber, fontSize: 25),
+                          textAlign: TextAlign.center,
+                        ));
+                  } else {
+                    return Container(color: Colors.green);
+                  }
+              }
+            }));
+  }
+}
+
+Widget buildTextField(
+    String label, String prefix, TextEditingController cont, Function fun) {
+  return TextField(
+    controller: cont,
+    onChanged: fun,
+    keyboardType: TextInputType.number,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25),
+  );
+}
